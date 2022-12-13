764f6072fc3c4c465d30f5aed1e2d75980d2a2df
SaleemHC <120365530+SaleemHC@users.noreply.github.com>
2022-12-13 17:00:47 +0530

    Update uploadassets.php

--- uploadassets.php
+++ uploadassets.php
@@ -1,72 +1,36 @@
+
+<?php
+
+include "connection.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Upload Assets</title>
+
     <style>
-        body{
-            text-align:center;
-        }
         .slt_tmp{
             width: 100%;
             display: flex;
             justify-content:center;
         }
-
         table{
-            width: 80%;
+            width: 80%; 
         }
-        table,th,td{
+        table,tr,td,th{
             border: 1px solid #000;
-            text-align: center;
         }
-        th{
-            padding: 10px 0;
-        }
-        td{
-            padding: 10px 0;
-            position: relative;
-        }
-
-
-        td .upd_ast{
-            position: absolute;
-            top: 0;
-            border: 1px solid #000;
-            display: none;
-            background: #fff;
-            padding: 0 12px;
-        }
-
-        td .upd_ast.active{
-            display: block;
-        }
-
-        .prv_bx{
-            width: 200px;
-        }
-        .prv{
-            width: 150px;
-            height: 150px;
-            /* object-fit: cover; */
-            border: 1px solid #000;
-            margin: 0 auto;
-            
-        }
-        .assets_req{
-            text-align: left;
-        }
-        .assets_req ul{
-            list-style: none;
-            padding-left: 0;
-        }
-    </style>    
+        /*     margin-top: -60px;
+    margin-left: -30px; */
+    </style>
 </head>
 <body>
     <div class="upload_cnt">
-        <h2>Upload Assets</h2>
-        <h3>Selected Template Name</h3>
+        <h2 style="text-align:center;">Upload Assets</h2>
+        <h3 style="text-align:center;">Selected Template Name</h3>
         <div class="slt_tmp">
             <table>
                 <thead>
@@ -78,24 +42,29 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>300x250</td>
-                        <td>Preview</td>
-                        <td>
-                            <button id="upload_asset" onclick="uploadAssets()">upload</button>
-                            <div class="upd_ast">
-                                <h5>Upload Assets</h5>
-                                <div class="assets_req">
-                                    <ul>
-                                        <li>background image</li>
-                                        <li>copy</li>
-                                        <li>cta</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </td>
-                        <td>alignment</td>
-                    </tr>
+                <?php
+                    $sql="SELECT * FROM `templates` WHERE id='1'";
+
+                    $data=mysqli_query($conn,$sql);
+                    if(mysqli_num_rows($data)>0){
+                        while($row=mysqli_fetch_assoc($data)){
+                            ?>
+                            <tr>
+                                <td style="text-align:center;"><?php echo $row['dim']; ?></td>
+                                <td style="position:relative;width:180px;height:180px;">
+                                    <div style="position:absolute;top:0;left:0;transform: scale(0.5);">
+                                        <?php echo $row['master_code']; ?>
+                                    </div>
+                                </td>
+                                <td style="text-align:center;">
+                                    <button id="upload_asset" onclick="uploadAssets()">upload</button>
+                                </td>
+                                <td style="text-align:center;">alignment</td>
+                            </tr>
+                            <?php
+                        }}               
+
+                ?>
                 </tbody>
             </table>
         </div>
